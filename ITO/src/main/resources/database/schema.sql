bf0c06a1cb4f99a6b643cf27f2334e75f0b5b9fe
Paloma <paloma.rocha01@fatec.sp.gov.br>
2025-06-18 22:36:08 -0300

    chore(db): cria schema para estrutura do banco de dados

---
+++ ITO/src/main/resources/database/schema.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE empresa;
+
+CREATE TABLE equipe {
+    equipe_id SERIAL PRIMARY KEY,
+    equipe_nome VARCHAR(100) NOT NULL,
+    equipe_setor VARCHAR(100) NOT NULL
+}
+
+CREATE TABLE funcionario {
+    fun_id SERIAL PRIMARY KEY,
+    fun_nome VARCHAR(100) NOT NULL,
+    fun_cargo VARCHAR(100) NOT NULL,
+    fun_email VARCHAR(100) NOT NULL,
+    equipe_id INT,
+    FOREIGN KEY (equipe_id) REFERENCES equipe(id)
+}
