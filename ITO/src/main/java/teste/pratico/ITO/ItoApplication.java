65cf0fcd95233cfc0804faddb42e9de404e90d22
Paloma <paloma.rocha01@fatec.sp.gov.br>
2025-06-18 21:15:11 -0300

    Iniciando o teste prático

---
+++ ITO/src/main/java/teste/pratico/ITO/ItoApplication.java
@@ -0,0 +1,13 @@
+package teste.pratico.ITO;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItoApplication.class, args);
+	}
+
+}
---
+++ ITO/src/test/java/teste/pratico/ITO/ItoApplicationTests.java
@@ -0,0 +1,13 @@
+package teste.pratico.ITO;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
