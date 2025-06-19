c6fb5b887b50030f731c8cdad34efd6f106ce70d
Paloma <paloma.rocha01@fatec.sp.gov.br>
2025-06-18 22:40:09 -0300

    feat(repository): cria interfaces repository

---
+++ ITO/src/main/java/teste/pratico/ITO/repository/FuncionarioRepository.java
@@ -0,0 +1,8 @@
+package teste.pratico.ITO.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import teste.pratico.ITO.entity.Funcionario;
+
+public interface FuncionarioRepository extends JpaRepository<Funcionario, Long> {
+
+}
