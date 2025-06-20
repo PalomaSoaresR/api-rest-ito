0331dc8148405bbebdf79d773917d9869d5b8d69
Paloma <paloma.rocha01@fatec.sp.gov.br>
2025-06-20 10:23:14 -0300

    chore(schema): define equipe_id no funcionario como NOT NULL

--- ITO/src/main/resources/database/schema.sql
+++ ITO/src/main/resources/database/schema.sql
@@ -1,16 +1,16 @@
 CREATE DATABASE empresa;
 
-CREATE TABLE equipe {
+CREATE TABLE equipe (
     equipe_id SERIAL PRIMARY KEY,
     equipe_nome VARCHAR(100) NOT NULL,
-    equipe_setor VARCHAR(100) NOT NULL
-}
+    equipe_setor VARCHAR(100) NOT NULL 
+    )
 
-CREATE TABLE funcionario {
+CREATE TABLE funcionario (
     fun_id SERIAL PRIMARY KEY,
     fun_nome VARCHAR(100) NOT NULL,
     fun_cargo VARCHAR(100) NOT NULL,
     fun_email VARCHAR(100) NOT NULL,
-    equipe_id INT,
+    equipe_id INT NOT NULL,
     FOREIGN KEY (equipe_id) REFERENCES equipe(id)
-}
+)
