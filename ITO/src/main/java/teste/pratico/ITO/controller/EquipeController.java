84cbf69621e1e35c92e60bd6c75f886431d60d79
Paloma <paloma.rocha01@fatec.sp.gov.br>
2025-06-22 23:19:50 -0300

    style(controller): remove imports e comentários desnecessários

--- ITO/src/main/java/teste/pratico/ITO/controller/EquipeController.java
+++ ITO/src/main/java/teste/pratico/ITO/controller/EquipeController.java
@@ -5,8 +5,6 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,7 +15,6 @@ import teste.pratico.ITO.entity.Equipe;
 import teste.pratico.ITO.service.EquipeService;
 
 @RestController
-@CrossOrigin
 @RequestMapping(value = "/equipes")
 public class EquipeController {
 
